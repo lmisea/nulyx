551d117bb7e100ff4b3037a172e7ffbdad21829c
Luis Isea <19-10175@usb.ve>
2023-07-23 18:09:36 -0400

    Only responding to authorized user and improved start message

--- src/bot.ts
+++ src/bot.ts
@@ -14,10 +14,30 @@ if (!TOKEN) {
 const bot: Telegraf = new Telegraf(TOKEN);
 
 // Start Command
-bot.start((ctx) => ctx.reply('Hello World!'));
+bot.start((ctx) => {
+	// Check if the chat id is not the one defined in the environment variables
+	if (ctx.chat.id !== parseInt(process.env.CHAT_ID)) {
+		process.exit(1);
+	}
+	// If the user is authorized, send a nice welcome message
+	ctx.reply(
+		`Hello, ${ctx.from.first_name}! 👋\nIt's an absolute pleasure to chat ` +
+			`with you.\nI am Nulyx, your dedicated personal assistant.\n\n` +
+			`Allow me to assist you with organizing your ideas and tasks, scheduling ` +
+			`activities, and providing tailored recommendations for coding projects ` +
+			`and anime series that match your interests.\n\n` +
+			`With my support, you'll be able to stay productive and explore new ` +
+			`horizons effortlessly. 🚀\n\nLet's embark on this exciting journey ` +
+			`together, and together we'll achieve great things! 🌟`
+	);
+});
 
 // Echo command
 bot.command('echo', (ctx) => {
+	// Check if the chat id is not the one defined in the environment variables
+	if (ctx.chat.id !== parseInt(process.env.CHAT_ID)) {
+		process.exit(1);
+	}
 	// Split the text message sent by the user
 	const message: string[] = ctx.message.text.split(' ');
 	// Remove the first element from array
