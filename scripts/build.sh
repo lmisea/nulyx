dfee590bc13c9e1ccb5789c7519cd3acda22c112
Luis Isea <19-10175@usb.ve>
2023-07-24 16:50:16 -0400

    Installing dependencies with 'npm run build'

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,22 @@
 # This script is used to build the project and it is meant to be
 # run from 'npm run build'.
 
+# Check if node_modules folder does not exist
+if [ ! -d "node_modules" ]; then
+    echo "Installing project dependencies..."
+    npm install
+fi
+
 # Check if dist folder does not exist
 if [ ! -d "dist" ]; then
+    echo "Creating dist folder..."
     mkdir dist
+fi
+
+# Check if aws_lambda/node_modules folder does not exist
+if [ ! -d "aws_lambda/node_modules" ]; then
+    echo "Installing aws_lambda function dependencies..."
+    cd aws_lambda/ && npm install
 fi
 
 # Compile the project
