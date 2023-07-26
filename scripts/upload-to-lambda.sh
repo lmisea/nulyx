fe36a9b21b9b798347bee5bef4210c8a45020e7e
Luis Isea <19-10175@usb.ve>
2023-07-26 10:33:08 -0400

    AWS SSO profile specified in package.json

--- scripts/upload-to-lambda.sh
+++ scripts/upload-to-lambda.sh
@@ -8,6 +8,15 @@
 # grant you the correct permissions.
 # And if the sso profile is not the correct one, please change it to the correct one.
 
+# Load aws profile from package.json
+AWS_PROFILE=$(grep "awsProfile" package.json | awk -F '"' '{print $4}')
+
+# Check if awsProfile is set
+if [ -z "$AWS_PROFILE" ]; then
+    echo "awsProfile is not set in package.json. Please set it first."
+    exit 1
+fi
+
 # Check if zip file exists
 if [ -f "aws_lambda/nulyx.zip" ]; then
     echo "Zip file located successfully!"
@@ -29,7 +38,7 @@ if ! ping -c 1 8.8.8.8 &> /dev/null; then
 fi
 
 # Check if AWS credentials are configured
-if ! aws sts get-caller-identity --profile aws-luis-admin &> /dev/null; then
+if ! aws sts get-caller-identity --profile $AWS_PROFILE &> /dev/null; then
     echo "AWS credentials could not be found."
     if [ ! -f "$HOME/.aws/config" ]; then
         echo "AWS config file could not be found."
@@ -37,14 +46,13 @@ if ! aws sts get-caller-identity --profile aws-luis-admin &> /dev/null; then
         exit 1
     fi
     echo "AWS config file found. Please login first."
-    aws sso login --profile aws-luis-admin
+    aws sso login --profile $AWS_PROFILE
 fi
 
 # Check if nulyxBot function exists
-if ! aws lambda get-function --function-name nulyxBot --profile aws-luis-admin &> /dev/null; then
+if ! aws lambda get-function --function-name nulyxBot --profile $AWS_PROFILE &> /dev/null; then
     echo "No function named nulyxBot found. Please create it using aws console online."
-    echo "Remember to set the handler to 'index.lambdaHandler' and adding the"
-    echo "two environment variables: 'BOT_TOKEN' and 'CHAT_ID'."
+    echo "Read the Deployment the first time section in the README.md file for more information."
     exit 1
 fi
 
@@ -52,7 +60,7 @@ fi
 echo "nulyxBot function located successfully. Uploading new zip file..."
 echo "Please press 'q' once the information of the function is displayed."
 
-if aws lambda update-function-code --function-name nulyxBot --zip-file fileb://aws_lambda/nulyx.zip --profile aws-luis-admin
+if aws lambda update-function-code --function-name nulyxBot --zip-file fileb://aws_lambda/nulyx.zip --profile $AWS_PROFILE
 then
     echo "Zip file uploaded sucessfully!"
 else
